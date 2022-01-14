d9ce508ad3411f7b3ef52d168331a68e8a0aeb4a
Ehsanul Hoque <ehsanul@ehsanul.com>
2022-01-14 12:20:10 -0500

    v1
    - check temp less often
    - tune for 12v input

--- diy-omd.ino
+++ diy-omd.ino
@@ -9,14 +9,14 @@ elapsedMillis timeMillis;
 elapsedMillis secondaryTimeMillis;
 elapsedMillis mpuTimeMillis;
 
-Servo ESC;     // create servo object to control the ESC
+Servo ESC; // create servo object to control the ESC
 const int AXE_550_CALIBRATE = 0;
 const int OMD = 1;
 const int MODE = OMD;
 const int hallEffectSensorPin = 14;
 const int stopValue = 90;
 const int reverseValue = 0;
-const int goValue = MODE == OMD ? 150 : 180;
+const int goValue = MODE == OMD ? 133 : 180;
 volatile int hallEffectCounter = 0;
 int revolutions = 0;
 int numGos = 0;
@@ -210,34 +210,20 @@ void loop() {
     }
   }
 
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-
-   Serial.println(temp.temperature);
-  // loop forever when too hot, ie require user to reboot so that they are in
-  // control of when it turns back on.
-  while (temp.temperature > 45.0) {
-    ESC.write(stopValue);
-    delay(500);
-  }
-
-  const float squareSum = a.acceleration.x * a.acceleration.x +
-    a.acceleration.y * a.acceleration.y +
-    a.acceleration.z * a.acceleration.z;
-  const float acc = sqrt(squareSum) - 9.81;
-
-  /* Print out the values */
-  if (mpuTimeMillis > 40) {
+  if (mpuTimeMillis > 60000) {
     mpuTimeMillis = 0;
-    //Serial.println(acc);
-    // Serial.print("Acceleration X: ");
-    // Serial.print(a.acceleration.x);
-    // Serial.print(", Y: ");
-    // Serial.print(a.acceleration.y);
-    // Serial.print(", Z: ");
-    // Serial.println(a.acceleration.z);
-    //Serial.println(" m/s^2");
+
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+     Serial.println(temp.temperature);
+    // loop forever when too hot, ie require user to reboot so that they are in
+    // control of when it turns back on.
+    while (temp.temperature > 45.0) {
+      ESC.write(stopValue);
+      delay(500);
+    }
   }
     
   if (timeMillis > 50) {
@@ -255,122 +241,3 @@ void loop() {
 void incrementHallEffectCount() {
   hallEffectCounter++;
 }
-
-
-//    // Basic demo for accelerometer readings from Adafruit MPU6050
-//    
-//    #include <Adafruit_MPU6050.h>
-//    #include <Adafruit_Sensor.h>
-//    #include <Wire.h>
-//    
-//    Adafruit_MPU6050 mpu;
-//    
-//    void setup(void) {
-//      Serial.begin(115200);
-//      while (!Serial)
-//        delay(10); // will pause Zero, Leonardo, etc until serial console opens
-//    
-//      Serial.println("Adafruit MPU6050 test!");
-//    
-//      // Try to initialize!
-//      if (!mpu.begin()) {
-//        Serial.println("Failed to find MPU6050 chip");
-//        while (1) {
-//          delay(10);
-//        }
-//      }
-//      Serial.println("MPU6050 Found!");
-//    
-//      mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-//      Serial.print("Accelerometer range set to: ");
-//      switch (mpu.getAccelerometerRange()) {
-//      case MPU6050_RANGE_2_G:
-//        Serial.println("+-2G");
-//        break;
-//      case MPU6050_RANGE_4_G:
-//        Serial.println("+-4G");
-//        break;
-//      case MPU6050_RANGE_8_G:
-//        Serial.println("+-8G");
-//        break;
-//      case MPU6050_RANGE_16_G:
-//        Serial.println("+-16G");
-//        break;
-//      }
-//      mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-//      Serial.print("Gyro range set to: ");
-//      switch (mpu.getGyroRange()) {
-//      case MPU6050_RANGE_250_DEG:
-//        Serial.println("+- 250 deg/s");
-//        break;
-//      case MPU6050_RANGE_500_DEG:
-//        Serial.println("+- 500 deg/s");
-//        break;
-//      case MPU6050_RANGE_1000_DEG:
-//        Serial.println("+- 1000 deg/s");
-//        break;
-//      case MPU6050_RANGE_2000_DEG:
-//        Serial.println("+- 2000 deg/s");
-//        break;
-//      }
-//    
-//      mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-//      Serial.print("Filter bandwidth set to: ");
-//      switch (mpu.getFilterBandwidth()) {
-//      case MPU6050_BAND_260_HZ:
-//        Serial.println("260 Hz");
-//        break;
-//      case MPU6050_BAND_184_HZ:
-//        Serial.println("184 Hz");
-//        break;
-//      case MPU6050_BAND_94_HZ:
-//        Serial.println("94 Hz");
-//        break;
-//      case MPU6050_BAND_44_HZ:
-//        Serial.println("44 Hz");
-//        break;
-//      case MPU6050_BAND_21_HZ:
-//        Serial.println("21 Hz");
-//        break;
-//      case MPU6050_BAND_10_HZ:
-//        Serial.println("10 Hz");
-//        break;
-//      case MPU6050_BAND_5_HZ:
-//        Serial.println("5 Hz");
-//        break;
-//      }
-//    
-//      Serial.println("");
-//      delay(100);
-//    }
-//    
-//    void loop() {
-//    
-//      /* Get new sensor events with the readings */
-//      sensors_event_t a, g, temp;
-//      mpu.getEvent(&a, &g, &temp);
-//    
-//      /* Print out the values */
-//      Serial.print("Acceleration X: ");
-//      Serial.print(a.acceleration.x);
-//      Serial.print(", Y: ");
-//      Serial.print(a.acceleration.y);
-//      Serial.print(", Z: ");
-//      Serial.println(a.acceleration.z);
-//      //Serial.println(" m/s^2");
-//    
-//      // Serial.print("Rotation X: ");
-//      // Serial.print(g.gyro.x);
-//      // Serial.print(", Y: ");
-//      // Serial.print(g.gyro.y);
-//      // Serial.print(", Z: ");
-//      // Serial.print(g.gyro.z);
-//      // Serial.println(" rad/s");
-//    
-//      // Serial.print("Temperature: ");
-//      // Serial.print(temp.temperature);
-//      // Serial.println(" degC");
-//    
-//      // Serial.println("");
-//      delay(50);
-//    }
